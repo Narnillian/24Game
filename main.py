9e6b4e454c86f853340748c67293f1792e029cf6
Narnillian <94580999+Narnillian@users.noreply.github.com>
2022-01-05 14:02:19 -0800

    Add files via upload
    Add main.py that I had already created on my computer before deciding to use GitHub

---
+++ main.py
@@ -0,0 +1,13 @@
+def find24(num1, num2, num3, num4):
+	baseArr=[num1, num2, num3, num4]
+    for i in *args:
+    	thisArr=[]
+    for i in combos:
+    	for j in 
+        
+
+    combos = [[num1, num2, num3, num4],[num2, num1, num3, num4], [num2, num3, num1, num4], [num2, num3, num4, num1], [num1, num3, num2, num4], [num1, num3, num4, num2], [num]]
+    
+    
+operations = ["+", "-", "*", "/"]
+find24(1, 3, 5, 7)
